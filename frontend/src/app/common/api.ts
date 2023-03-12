744479ab40a9579276601763e0fce1356b09ec5c
alinkakhu <alinakhu9@gmail.com>
2023-03-12 13:42:46 +0100

    feat:add dictionary

--- frontend/src/app/common/api.ts
+++ frontend/src/app/common/api.ts
@@ -1,5 +1,6 @@
 import { AuthManager } from "./auth.manager";
 import { Config } from "./config";
+
 export class Api {
     static instance: Api;
     baseUrl: string;
@@ -9,42 +10,59 @@ export class Api {
 
     static getInstance() {
         if (!Api.instance) {
-            Api.instance = new Api('http://localhost:3000');
+            Api.instance = new Api(Config.ApiUrl);
         }
         return Api.instance;
     }
 
     async get(endpoint: string) {
         const url = new URL(endpoint, this.baseUrl);
-        return await (await fetch(url.toString())).json();
+        return await (await this.ensureSuccess(await fetch(url.toString(), {
+            headers: {
+                "Authorization": "Bearer " + AuthManager.getInstance().getToken()
+            }
+        }))).json();
     }
 
     async post(endpoint: string, data: any) {
         const url = new URL(endpoint, this.baseUrl);
-        return await (await fetch(url.toString(), {
+        return await (await this.ensureSuccess(await fetch(url.toString(), {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                "Authorization": "Bearer " + AuthManager.getInstance().getToken()
             },
             body: JSON.stringify(data)
-        })).json();
+        }))).json();
     }
 
     async put(endpoint: string, data: any) {
         const url = new URL(endpoint, this.baseUrl);
-        return await (await fetch(url.toString(), {
+        return await (await this.ensureSuccess(await fetch(url.toString(), {
             method: 'PUT',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                "Authorization": "Bearer " + AuthManager.getInstance().getToken()
             },
             body: JSON.stringify(data)
-        })).json();
+        }))).json();
     }
 
-    async delete(endpoint: string, args: any[]) {
+    async delete(endpoint: string) {
         const url = new URL(endpoint, this.baseUrl);
-        return await (await fetch(url.toString(), {
-            method: 'DELETE'
-        })).json();
+        return await (await this.ensureSuccess(await fetch(url.toString(), {
+            method: 'DELETE',
+            headers: {
+                "Authorization": "Bearer " + AuthManager.getInstance().getToken()
+            }
+        }))).json();
+    }
+
+    async ensureSuccess(response: Response) {
+        if (!response.ok) {
+            const json = await response.json();
+            throw new Error(json.message || json.error || response.statusText);
+        }
+        return response;
     }
 }
